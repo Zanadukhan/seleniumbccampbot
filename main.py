577b61f463bdfab880ef06c55852a687de824df5
Zanadukhan <cchiu231998@hotmail.ca>
2023-04-02 21:02:28 -0700

    Optimized imports to follow pep8 guidelines

--- main.py
+++ main.py
@@ -1,10 +1,14 @@
+import time
+import string
+import os
 from selenium import webdriver
 from selenium.webdriver.firefox.service import Service as FirefoxService
 from webdriver_manager.firefox import GeckoDriverManager
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.ui import WebDriverWait
-import time, string, os
+
+
 
 PARK = string.capwords('E.C. Manning')
 MONTH = 'jun'.capitalize()
